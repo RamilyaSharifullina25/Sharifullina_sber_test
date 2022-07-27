168d3706ef9fb306a0f60dd5e012384deb15651b
RamilyaSharifullina25 <sharifullina.ramilya@bk.ru>
2022-07-27 21:09:24 +0300

    edited app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-
+import os
 import torch
 import torch.nn as nn
 from torchvision import transforms, models
 from PIL import Image
 import io
 from flask import Flask, redirect, request, render_template, jsonify
+from werkzeug.utils import secure_filename
 
 MODEL_PATH = 'resnet_pretrained.pt'
 
@@ -24,23 +25,26 @@ def transform_image(image_bytes):
     image = Image.open(io.BytesIO(image_bytes))
     return my_transforms(image).unsqueeze(0)
 
-model = models.densenet121(pretrained=True)
+model = models.resnet18(weights = True)
 model.eval()
 
 def get_prediction(image_bytes):
     tensor = transform_image(image_bytes=image_bytes)
-    outputs = model.forward(tensor)
+    outputs = model(tensor)
     _, y_hat = outputs.max(1)
     return y_hat
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
-    if request.method == 'POST':
-        # Get the file from post request
-        f = request.files['file']
-        img_bytes = file.read()
-        class_id, class_name = get_prediction(image_bytes=img_bytes)
-        return jsonify({'class_id': class_id, 'class_name': class_name})
+	if request.method == 'POST':
+		f = request.files['file']
+		basepath = os.path.dirname(__file__)
+        file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+        out = transform_image(file_path)
+        return str(out)
+	return None 
+
 
 if __name__ == '__main__':
     app.run(debug=True)
