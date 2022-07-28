4052727c7a1df2ff96500191c3108965f9adb4de
RamilyaSharifullina25 <sharifullina.ramilya@bk.ru>
2022-07-28 11:00:33 +0300

    added

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import io
 from flask import Flask, redirect, request, render_template, jsonify
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'static/uploads/')
 ALLOWED_EXTENTIONS = {'png', 'jpg', 'jpeg'} 
 MODEL_PATH = 'resnet_pretrained.pt'
 
@@ -19,20 +19,18 @@ def index():
     return render_template('index.html')
 
 def transform_image(image):
-    my_transforms = transforms.Compose([transforms.Resize(255),
-					transforms.CenterCrop(224),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize(
-                                            [0.485, 0.456, 0.406],
-                                            [0.229, 0.224, 0.225])])
-    img = Image.open(io.BytesIO(image))
-    return my_transforms(img).unsqueeze(0)
+	my_transforms = transforms.Compose([transforms.CenterCrop(256),
+                                transforms.ToTensor(),
+                                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                                ])
+	img = Image.open(image).convert('RGB')
+	return my_transforms(img).unsqueeze(0)
 
 def get_prediction(image, model):
-    tensor = transform_image(image=image)
-    outputs = model(tensor)
-    _, y_hat = outputs.max(1)
-    return y_hat
+	tensor = transform_image(image=image)
+	outputs = model(tensor)
+	_, y_hat = outputs.max(1)
+	return y_hat
 
 resnet = models.resnet18(weights = True)
 resnet.eval()
@@ -41,10 +39,10 @@ resnet.eval()
 def upload():
 	if request.method == 'POST':
 		f = request.files['file']
-		file_path = os.path.join(os.path.dirname(__file__), 'uploads' , secure_filename(f.filename))
+		file_path = os.path.join(app.config['UPLOAD_FOLDER'],  secure_filename(f.filename))
 		f.save(file_path)
 		result = get_prediction(file_path, model = resnet)
-		return result
+		return str(result)
 	return None
 
 if __name__ == '__main__':
